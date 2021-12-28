a59e8fd801e33a03ac50a8749668ecdc3407dec7
Jan <jamo.gabriel@gmail.com>
2021-12-28 10:01:20 +0100

    initial setup frontend

---
+++ frontend/target/debug/deps/frontend-7c92796acd8db0d0.d
@@ -0,0 +1,5 @@
+C:\Users\birdi\Documents\HDA\Advanced System Programming\project_lab4\lab4-6\frontend\target\debug\deps\frontend-7c92796acd8db0d0.exe: src\main.rs
+
+C:\Users\birdi\Documents\HDA\Advanced System Programming\project_lab4\lab4-6\frontend\target\debug\deps\frontend-7c92796acd8db0d0.d: src\main.rs
+
+src\main.rs:
---
+++ frontend/target/debug/frontend.d
@@ -0,0 +1,1 @@
+C:\Users\birdi\Documents\HDA\Advanced\ System\ Programming\project_lab4\lab4-6\frontend\target\debug\frontend.exe: C:\Users\birdi\Documents\HDA\Advanced\ System\ Programming\project_lab4\lab4-6\frontend\src\main.rs
