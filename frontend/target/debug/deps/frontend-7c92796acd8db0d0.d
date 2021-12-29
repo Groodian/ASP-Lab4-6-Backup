adc3e12a338f341f1294e5bb88492b8e005f6f48
Jan <jamo.gabriel@gmail.com>
2021-12-29 14:20:31 +0100

    added gitignore and removed target folder

--- frontend/target/debug/deps/frontend-7c92796acd8db0d0.d
+++
@@ -1,5 +0,0 @@
-C:\Users\birdi\Documents\HDA\Advanced System Programming\project_lab4\lab4-6\frontend\target\debug\deps\frontend-7c92796acd8db0d0.exe: src\main.rs
-
-C:\Users\birdi\Documents\HDA\Advanced System Programming\project_lab4\lab4-6\frontend\target\debug\deps\frontend-7c92796acd8db0d0.d: src\main.rs
-
-src\main.rs:
--- frontend/target/debug/deps/frontend.d
+++
@@ -1,5 +0,0 @@
-C:\Users\birdi\Documents\HDA\Advanced System Programming\project_lab4\lab4-6\frontend\target\debug\deps\frontend.exe: src\main.rs
-
-C:\Users\birdi\Documents\HDA\Advanced System Programming\project_lab4\lab4-6\frontend\target\debug\deps\frontend.d: src\main.rs
-
-src\main.rs:
